62424a6d1e981815193c06c014f2cd82c8e6f6b1
Aidan Bustos <a@bustos.org>
2019-10-16 12:48:22 -0400

    added youtube
    !youtube (search query)

--- bryan the bird test.py
+++ bryan the bird test.py
@@ -87,7 +87,15 @@ async def giphy(ctx, *, search):
     await session.close()
 
     await client.send_message(embed=embed)
-    
+   
+@bot.command(pass_context=True)
+async def youtube(ctx, *search):
+    searchTerm = ' '.join(search)
+    query_string = urllib.parse.urlencode({'search_query': searchTerm})
+    htm_content = urllib.request.urlopen('https://www.youtube.com/results?' + query_string)
+    search_results = re.findall('href=\"\\/watch\\?v=(.{11})', htm_content.read().decode())
+    await ctx.send('http://www.youtube.com/watch?v=' + search_results[0])
+
 if __name__ == '__main__':
     import config
     bot.run(config.token)
