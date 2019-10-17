13b008586836f0de836c583881a1cbfcf714bb29
Aidan Bustos <a@bustos.org>
2019-10-17 10:52:57 -0400

    added wip code for level system
    right now this code does not function correctly but I thought I would throw it in for the time being because it is a nice start for anyone to start a level system. This code will likely be replaced in the near future because I have some ideas to make it better.

--- bryan the bird test.py
+++ bryan the bird test.py
@@ -25,6 +25,53 @@ async def brozart(ctx):
 # async def serverinvite(ctx):
 #   await ctx.send('invite link: (your server invite)')
 
+
+
+@bot.event
+async def on_message(message):
+    with open("users.json", "r") as f:
+        users = json.load(f)
+
+        if message.author.bot:
+            return
+        if message.channel.is_private:
+            return
+        else:
+            await update_data(users, message.author, message.server)
+            number = random.randint(5,10)
+            await add_experience(users, message.author, number, message.server)
+            await level_up(users, message.author, message.channel, message.server)
+
+        with open("users.json", "w") as f:
+            json.dump(users, f)
+    await bot.process_commands(message)
+
+async def update_data(users, user, server):
+    if not user.id + "-" + server.id in users:
+        users[user.id + "-" + server.id] = {}
+        users[user.id + "-" + server.id]["experience"] = 0
+        users[user.id + "-" + server.id]["level"] = 1
+        users[user.id + "-" + server.id]["last_message"] = 0
+
+async def add_experience(users, user, exp, server):
+    if time.time() - users[user.id + "-" + server.id]["last_message"] > 30: 
+        users[user.id + "-" + server.id]["experience"] += exp
+        users[user.id + "-" + server.id]["last_message"] = time.time()
+    else:
+        return
+
+async def level_up(users, user, channel, server):
+    experience = users[user.id + "-" + server.id]["experience"]
+    lvl_start = users[user.id + "-" + server.id]["level"]
+    lvl_end = int(experience ** (1/4))
+
+    if lvl_start < lvl_end:
+        await bot.send_message(channel, f":tada: Congrats {user.mention}, you levelled up to level {lvl_end}!")
+        users[user.id + "-" + server.id]["level"] = lvl_end
+
+
+
+
 @bot.command()
 async def coords(ctx, xCoord: str, yCoord: str, zCoord: str, nameCoord: str = None):
     if not xCoord or not yCoord or not zCoord or not nameCoord:
