8f69f4271afc0c497b84c1d531cdd9209dc5711c
Aidan Bustos <a@bustos.org>
2019-10-23 12:40:28 -0400

    Update August first .py

--- August first .py
+++ August first .py
@@ -15,127 +15,47 @@ async def on_ready():
     print('------')
 
 
+@client.event
+async def on_member_join(member):
+    with open('users.json','r') as f:
+        users = json.load(f)
 
-@bot.command()
-async def test(ctx):
-    await ctx.send_file('brozart.jpg')
+    await update_data(users, member)
 
+    with open('users.json','w') as f:
+        json.dump(users, f)
 
-@bot.command()
-async def cat(ctx):
-    await ctx.send("https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")
+@client.event 
+async def on_message(message):
+    with open('users.json','r') as f:
+        users = json.load(f)
 
-@bot.command()
-async def info(ctx):
-    embed = discord.Embed(title="Server Bot", description="Worst bot ever", color=0xeee657)
-
-    
-    embed.add_field(name="Author", value="Cat")
-
-
-    embed.add_field(name="prefix", value="?")
-
-    embed.add_field(name="Server ip", value="")
-
-    await ctx.send(embed=embed)
-
-@bot.command()
-async def up(ctx):
-    await bot.change_presence(activity=discord.Game(name='Server is Online | ?help'))
-
-@bot.command()
-async def down(ctx):
-    await bot.change_presence(activity=discord.Game(name='Server is Offline | ?help'))
-    await ctx.send(" <@296444389990072320> will show you the way")
+    await update_data(users, message.author)
+    await add_experience(users, message.author, 5)
+    await level_up(users, message.author, message.channel)
 
 
-    await ctx.send(embed=embed)
+    with open('users.json','w') as f:
+        json.dump(users, f)
+
+async def update_data(users, user):
+    if not user.id in users:
+        
+        users[user.id] = {}
+        users[user.id]['experience'] = 0
+        users[users.id]['level'] = 1
+
+async def add_experience(users, user, exp):
+    users[user.id]['experience'] += exp
+
+async def level_up(users, user, channel):
+    experience = users[user.id]['experience']
+    lvl_start = users[user.id]['level']
+    lvl_end = int(experience ** (1/4))
 
 
-
-@bot.command()
-async def skyfactory(ctx):
-    embed = discord.Embed(title="Skyfactory Download", url="https://www.feed-the-beast.com/projects/ftb-presents-skyfactory-3/files/2481283", description="Here you can download the modpack", color=0xeee657)
-
-    # give info about you here
-    embed.set_thumbnail(url="https://media.forgecdn.net/avatars/83/195/636186954657341781.png")
-
-
-
-    embed.add_field(name="Server Ip", value="[redacted]]", inline=False)
-
-    await ctx.send(embed=embed)
-
-bot.remove_command('help')
-
-@bot.command()
-async def help(ctx):
-    embed = discord.Embed(title="Bot Commands", description=" List of commands are:", color=0xeee657)
-
-    embed.add_field(name="?add X Y", value="Gives the addition of **X** and **Y**", inline=False)
-    embed.add_field(name="?multiply X Y", value="Gives the multiplication of **X** and **Y**", inline=False)
-    embed.add_field(name="?greet", value="Gives a nice greet message", inline=False)
-    embed.add_field(name="?cat", value="Gives a cute cat gif to lighten up the mood.", inline=False)
-    embed.add_field(name="?invite", value="Gives an invite link for the bot", inline=False)
-    embed.add_field(name="?help", value="Gives this message", inline=False)
-    embed.add_field(name="?skyfactory", value="Gives info about skyfactory server", inline=False)
-    embed.add_field(name="?up", value="Toggles the bot server status on", inline=False)
-    embed.add_field(name="?down", value="toggles the bot server status off", inline=False)
-    embed.add_field(name="?hamachi", value="Shows you where to download hamachi and our current servers", inline=False)
-    embed.add_field(name="?[redacted]", value="[redacted]", inline=False)
-    embed.add_field(name="?[redacted]", value="[redacted] ", inline=False)
-    embed.add_field(name="?[redacted]", value="[redacted] ", inline=False)
-    embed.add_field(name="?giphy", value="search giphy. example: ?giphy cat (Must include a space)", inline=False)
-    embed.add_field(name="?coords", value="Submit a coord for a location(format xCoord yCoord zCoord nameCoord", inline=False)
-    embed.add_field(name="?reqcoord", value="Request a coord for a location (example: ?reqcoord (name of coord)", inline=False)
-    embed.add_field(name="?youtube", value="search youtube. example: ?youtube minercaft (Must include a space)", inline=False)
-
-    await ctx.send(embed=embed)
-
-    
-
-
-@bot.command()
-async def invite(ctx):
-    embed = discord.Embed(title="Invite Link", url="https://discordapp.com/api/oauth2/authorize?client_id=562970733781844008&permissions=8&scope=bot", description="Here you can invite me to your server", color=0xeee657)
-
-    # give info about you here
-    embed.set_thumbnail(url="https://upload.wikimedia.org/wikipedia/commons/f/f3/Erithacus_rubecula_with_cocked_head.jpg")
-
-
-
-    embed.add_field(name="Requirements", value="jack shit, just let me send and read messages", inline=False)
-
-    await ctx.send(embed=embed)
-
-@bot.command(pass_context=True)
-async def youtube(ctx, *search):
-    searchTerm = ' '.join(search)
-    query_string = urllib.parse.urlencode({'search_query': searchTerm})
-    htm_content = urllib.request.urlopen('https://www.youtube.com/results?' + query_string)
-    search_results = re.findall('href=\"\\/watch\\?v=(.{11})', htm_content.read().decode())
-    await ctx.send('http://www.youtube.com/watch?v=' + search_results[0])
-
-
-
-@bot.command()
-async def giphy(ctx, *, search):
-    embed = discord.Embed(colour=discord.Colour.blue())
-    session = aiohttp.ClientSession()
-
-    if search == '':
-        response = await session.get('https://api.giphy.com/v1/gifs/random?api_key=API_KEY_GOES_HERE')
-        embed.set_image(url=data['data']['images']['original']['url'])
-    else:
-        search.replace(' ', '+')
-        response = await session.get('http://api.giphy.com/v1/gifs/search?q=' + search + '&api_key=API_KEY_GOES_HERE&limit=10')
-        gif_choice = random.randint(0, 9)
-        embed.set_image(url=data['data'][gif_choice]['images']['original']['url'])
-
-    data = json.loads(await response.text())
-    await session.close()
-
-    await client.send_message(embed=embed)
-
+if lvl_start < lvl_end:
+    await client.send_message(channel, '{} has leveled up to level {}'.format(user.mention, lvl_end))
+    users[user.id]['level'] = lvl.end
 
 bot.run('NTk0NjYxOTI3ODQyNzQyMzA0.XRgYCQ.jB6bqDs42S34l8GKgsSZc8O3Q0s')
