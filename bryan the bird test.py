bcc4b816bcc754cf4db1d94457126f17f3da7289
Aidan Bustos <a@bustos.org>
2019-10-29 08:48:57 -0400

    Removed level command
    Broken for right now, Might come back to it and work on it

--- bryan the bird test.py
+++ bryan the bird test.py
@@ -40,42 +40,41 @@ async def coord(ctx):
 # async def serverinvite(ctx):
 #   await ctx.send('invite link: (your server invite)')
 
+#this command is for user levels but I dont know how to make it function correctly so it is on hold for the momment
 
-
-@bot.event
-async def on_message(message):
-    with open("users.json", "r") as f:
-        users = json.load(f)
-
-        if message.author.bot:
-            return
-        else:
-            await update_data(users, message.author)
-            number = random.randint(5,10)
-            await add_experience(users, message.author, number)
-            await level_up(users, message.author, message.channel)
-
-        with open("users.json", "w") as f:
-            json.dump(users, f)
-    await bot.process_commands(message)
-
-async def update_data(users, user):
-    if not user.id in users:
-        users[str(user.id)] = {}
-        users[str(user.id)]["experience"] = 0
-        users[str(user.id)]["level"] = 1
-
-async def add_experience(users, user, exp):
-    users[str(user.id)]["experience"] += exp
-
-async def level_up(users, user, channel):
-    experience = users[str(user.id)]["experience"]
-    lvl_start = users[str(user.id)]["level"]
-    lvl_end = int(experience ** (1/4))
-
-    if lvl_start < lvl_end:
-        await channel.send(f":tada: Congrats {user.mention}, you levelled up to level {lvl_end}!")
-        users[str(user.id)]["level"] = lvl_end
+#@bot.event
+#async def on_message(message):
+#    with open("users.json", "r") as f:
+#        users = json.load(f)
+#
+#        if message.author.bot:
+#            return
+#        else:
+#            await update_data(users, message.author)
+#            number = random.randint(5,10)
+#            await level_up(users, message.author, message.channel)
+#
+#        with open("users.json", "w") as f:
+#            json.dump(users, f)
+#    await bot.process_commands(message)
+#
+#async def update_data(users, user):
+#    if not user.id in users:
+#        users[str(user.id)] = {}
+#        users[str(user.id)]["experience"] = 0
+#        users[str(user.id)]["level"] = 1
+#
+#async def add_experience(users, user, exp):
+#    users[str(user.id)]["experience"] += exp
+#
+#async def level_up(users, user, channel):
+#    experience = users[str(user.id)]["experience"]
+#    lvl_start = users[str(user.id)]["level"]
+#   lvl_end = int(experience ** (1/4))
+#
+#    if lvl_start < lvl_end:
+#       await channel.send(f":tada: Congrats {user.mention}, you levelled up to level {lvl_end}!")
+#        users[str(user.id)]["level"] = lvl_end
 
 
 
